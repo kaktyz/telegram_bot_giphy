a8b8eea2279ec1eec2f77068e0c1d4411cb90a70
Anton Haustov <anton.haustov@borodino.local>
2020-06-27 18:07:24 +0300

    Testing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { Telegraf } = require('telegraf')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const fs = require('fs')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+
+const bot = new Telegraf("token");
+
+
+bot.start((ctx) => ctx.reply('Привет, я бот для поиска GIF-ок, напиши любое слово, и я поищу что-то интересное специально для тебя.'));
+bot.help((ctx) => ctx.reply('Пришли мне стикер'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('джейк', (ctx) => ctx.replyWithAnimation('https://99px.ru/sstorage/86/2017/07/image_86100717001237906221.gif'));
+
+
 bot.launch()
