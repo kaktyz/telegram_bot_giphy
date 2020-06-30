b3a1dee72349c6479a46c9afc18cec07e6640e2a
kaktyz <tatowkin@mail.ru>
2020-06-30 15:43:13 +0300

    Change telegram API

--- index.js
+++ index.js
@@ -1,71 +1,46 @@
-const Telegraf = require('telegraf')
-const giphy = require('giphy-api')('ugaftL4Bbn7PRk8kzctvHsfVeSq4iNuM');
-// const bot = new Telegraf("1144440917:AAEBfc77q_c7-dH_4OjWcS9WuQmwoMrNl1o");
-const bot = new Telegraf("882893548:AAG4QhL3oJ0ybzIY7PbwKggch79V4wFyx0o");
+// Подключаем библиотеку для работы с Telegram API в переменную
+var TelegramBot = require('node-telegram-bot-api');
 
-
-bot.start((ctx) => ctx.reply('Привет, я бот для поиска GIF-ок, напиши любое слово, и я поищу что-то интересное специально для тебя.'));
-bot.help((ctx) => ctx.reply('Напиши слово и я найду прикольную гифку для тебя'));
-bot.hears('Слава', (ctx) => {return ctx.replyWithAnimation('https://i.gifer.com/OJOg.gif')});
+// Устанавливаем токен, который выдавал нам бот
+var token = '1144440917:AAEBfc77q_c7-dH_4OjWcS9WuQmwoMrNl1o';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
 
 let settings = {
-    "api" : "https://api.giphy.com/v1/gifs/search?",
+    "api" : "https://api.giphy.com/v1/gifs/random?",
     "apiKey" : "api_key=ugaftL4Bbn7PRk8kzctvHsfVeSq4iNuM",
-    "query" : "&q=",
-    "limit" : "&limit=10",
-    "rating" : "&rating=G",
-    "language" : "&lang=en"
+    "query" : "&tag="
 };
 
-bot.use((ctx) => {
+function getGif(link) {
 
-    let message = ctx.message.text;
-    let urlOfGif =  settings.api + settings.apiKey + settings.query + message + settings.limit + settings.rating + settings.language;
+        // // Забираем данные с сылки
+        const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", link, false);
+        xhr.send(null);
+        
+        // // Парсим полученные данные с ссылки
+        let res = JSON.parse(xhr.responseText);
 
-    console.log("Текст " + message)
-    console.log("урл " + urlOfGif)
+        return res.data.images.original.url;
+}
 
-    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;  
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", urlOfGif, false);
-    xhr.send(null);
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/day (.+)/i, function (msg, match) {
     
-    let res = JSON.parse(xhr.responseText);
-
-    let rand = Math.floor(Math.random() * 10);
-    let gif = res.data[rand].images.original.url;
-/**
- * TODO: изменить приходящую кодировку на UTF8
- */
-    // const encodedGif = encodeURI(gif);
-    // return ctx.replyWithAnimation(`${encodedGif}`)
-
-    return ctx.replyWithAnimation(`${gif}`)
+    var fromId = msg.chat.id; // Получаем ID отправителя
+    let urlOfGif = encodeURI(settings.api + 
+        settings.apiKey + 
+        settings.query + 
+        match[1]
+        );
     
-})
 
-// bot.use((ctx) => {
 
-//     let message = ctx.message.text;
-    
-//     giphy.random(message).then(function (res) {
+    let gif = getGif(urlOfGif);
 
-//             console.log(res);
-// /**
-//  * TODO: возвращает не JSON, разобраться
-//  */
-//             let result = JSON.parse(res.responseText)
-//             return ctx.replyWithAnimation(`${result.data.images.original}`)
-      
-//     });
-    
-// })
+    bot.sendDocument(fromId, gif);
 
-/*
-
-    https://api.giphy.com/v1/gifs/search?api_key=ugaftL4Bbn7PRk8kzctvHsfVeSq4iNuM&q=rainbow&limit=1&offset=0&rating=G&lang=en
-    https://api.giphy.com/v1/gifs/search?api_key=ugaftL4Bbn7PRk8kzctvHsfVeSq4iNuM&q=love&limit=1&lang=en",
-
-*/
-
-bot.launch()
+});
